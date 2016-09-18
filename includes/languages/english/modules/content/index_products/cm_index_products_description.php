c89d0ac588bb300fcf71fcaf4b7197a50e2e89a4
ocs1409 <nieuw1409@users.noreply.github.com>
2016-09-18 21:40:12 +0200

    Update cm_index_products_description.php

--- includes/languages/english/modules/content/index_products/cm_index_products_description.php
+++ includes/languages/english/modules/content/index_products/cm_index_products_description.php
@@ -5,7 +5,7 @@
   Copyright (c) 2014 osCommerce
   Released under the GNU General Public License
 */
-  define('MODULE_CONTENT_INDEX_CATEGORIES_DESCRIPTION_TITLE', 'Index Products Page Description Block');
-  define('MODULE_CONTENT_INDEX_CATEGORIES_DESCRIPTION_DESCRIPTION', 'Adds an Products Description  to the Index Products Page of your site');
+  define('MODULE_CONTENT_INDEX_PRODUCTS_DESCRIPTION_TITLE', 'Index Products Page Description Block');
+  define('MODULE_CONTENT_INDEX_PRODUCTS_DESCRIPTION_DESCRIPTION', 'Adds an Products Description  to the Index Products Page of your site');
 
 ?>
