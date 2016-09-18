2c220db9ce1e9dba2fd356d62e1cc5ee09832457
ocs1409 <nieuw1409@users.noreply.github.com>
2016-09-18 21:37:26 +0200

    Update cm_pi_reviews.php

--- includes/languages/english/modules/content/product_info/cm_pi_reviews.php
+++ includes/languages/english/modules/content/product_info/cm_pi_reviews.php
@@ -6,8 +6,8 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_PRODUCT_INFO_REVIEWS_TITLE', 'Product Info Reviews');
-  define('MODULE_CONTENT_PRODUCT_INFO_REVIEWS_DESCRIPTION', 'Show review block on the product info page.');
+  define('MODULE_CONTENT_PI_REVIEWS_TITLE', 'Product Info Reviews');
+  define('MODULE_CONTENT_PI_REVIEWS_DESCRIPTION', 'Show review block on the product info page.');
 
-  define('MODULE_CONTENT_PRODUCT_INFO_REVIEWS_TEXT_RATED', 'Rated %s by <cite title="%s">%s</cite>');
+  define('MODULE_CONTENT_PI_REVIEWS_TEXT_RATED', 'Rated %s by <cite title="%s">%s</cite>');
 ?>
