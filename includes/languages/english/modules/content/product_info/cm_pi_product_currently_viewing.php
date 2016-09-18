837f856ac7b2609c2f44772d680b8e17df61b258
ocs1409 <nieuw1409@users.noreply.github.com>
2016-09-18 21:33:38 +0200

    Update cm_pi_product_currently_viewing.php

--- includes/languages/english/modules/content/product_info/cm_pi_product_currently_viewing.php
+++ includes/languages/english/modules/content/product_info/cm_pi_product_currently_viewing.php
@@ -6,7 +6,7 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCT_CURRENTLY_VIEWING_TITLE', 'Product Info Description');
-  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCT_CURRENTLY_VIEWING_DESCRIPTION', 'Show Product Description on the product info page.');
+  define('MODULE_CONTENT_PI_PRODUCT_CURRENTLY_VIEWING_TITLE', 'Product Info Description');
+  define('MODULE_CONTENT_PI_PRODUCT_CURRENTLY_VIEWING_DESCRIPTION', 'Show Product Description on the product info page.');
 
 ?>
