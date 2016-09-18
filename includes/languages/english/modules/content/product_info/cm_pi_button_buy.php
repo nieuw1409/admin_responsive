1eccce85f2700b3886cf8d4cafe78130968a3cd7
ocs1409 <nieuw1409@users.noreply.github.com>
2016-09-18 21:30:14 +0200

    Update cm_pi_button_buy.php

--- includes/languages/english/modules/content/product_info/cm_pi_button_buy.php
+++ includes/languages/english/modules/content/product_info/cm_pi_button_buy.php
@@ -6,7 +6,7 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCT_BUTTON_BUY_TITLE', 'Product Info Button Buy');
-  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCT_BUTTON_BUY_DESCRIPTION', 'Show Product Button Buy on the product info page.');
+  define('MODULE_CONTENT_PI_PRODUCT_BUTTON_BUY_TITLE', 'Product Info Button Buy');
+  define('MODULE_CONTENT_PI_PRODUCT_BUTTON_BUY_DESCRIPTION', 'Show Product Button Buy on the product info page.');
 
 ?>
