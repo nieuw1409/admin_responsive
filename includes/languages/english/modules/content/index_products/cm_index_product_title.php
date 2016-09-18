0e05371d019670ed89026ae7696a0e04453f549d
ocs1409 <nieuw1409@users.noreply.github.com>
2016-09-18 21:39:44 +0200

    Update cm_index_product_title.php

--- includes/languages/english/modules/content/index_products/cm_index_product_title.php
+++ includes/languages/english/modules/content/index_products/cm_index_product_title.php
@@ -5,7 +5,7 @@
   Copyright (c) 2014 osCommerce
   Released under the GNU General Public License
 */
-  define('MODULE_CONTENT_INDEX_CATEGORIES_TITLE_TITLE', 'Index Products Page Title Block');
-  define('MODULE_CONTENT_INDEX_CATEGORIES_TITLE_DESCRIPTION', 'Adds an  Prducts Title to the Index Products Page of your site');
+  define('MODULE_CONTENT_INDEX_PRODUCTS_TITLE_TITLE', 'Index Products Page Title Block');
+  define('MODULE_CONTENT_INDEX_PRODUCTS_TITLE_DESCRIPTION', 'Adds an  Prducts Title to the Index Products Page of your site');
 
 ?>
